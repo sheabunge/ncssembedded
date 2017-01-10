ad489c07634b0c08ddb6bc4c3700e73aa34af041
Shea Bunge <bungeshea@gmail.com>
2017-01-10 14:49:35 +1100

    Add files for controller and glove

---
+++ controller.py
@@ -0,0 +1,3 @@
+from microbit import *
+
+# this code will run on the 'master' microbit which manages the lights, etc
---
+++ golve.py
@@ -0,0 +1,3 @@
+from microbit import *
+
+# this file runs on the 'slave' microbit, which will be mounted on a glove

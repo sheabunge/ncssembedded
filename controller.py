966e8f32fba6820dca3b617bea4909c16cd4f37f
Shea Bunge <bungeshea@gmail.com>
2017-01-10 17:43:33 +1100

    Add controller and glove files

--- controller.py
+++ controller.py
@@ -1,3 +1,33 @@
 from microbit import *
+from neopixel import NeoPixel
+import radio
 
 # this code will run on the 'master' microbit which manages the lights, etc
+
+LIGHTS = 30
+COLORS = [(255, 0, 0)] * 9 + [(0, 0, 255)] * 10 + [(0, 255, 0)] * 11
+
+
+def light_percent(np, percent, colors):
+	length = len(np)
+	number_on = min(int(percent / 100 * length), length)
+	print('turning', number_on, 'lights on')
+	np.clear()
+
+	for pixel in range(number_on):
+		np[pixel] = colors[pixel]
+	np.show()
+
+
+radio.on()
+radio.config(channel=97)
+np = NeoPixel(pin0, LIGHTS)
+
+while True:
+	message = radio.receive()
+	if not message:
+		continue
+
+	speed = int(message) // 400
+	print(speed)
+	light_percent(np, speed, COLORS)
--- golve.py
+++ golve.py
@@ -1,3 +1,24 @@
 from microbit import *
+import radio
 
 # this file runs on the 'slave' microbit, which will be mounted on a glove
+
+AIR_RESISTANCE = 0.98
+
+radio.on()
+radio.config(channel=97)
+
+speed = 0
+
+while True:
+	display.clear()
+	acc = accelerometer.get_x() // 16
+
+	if acc > 31:
+		display.show(Image.ARROW_N)
+		speed += acc
+
+	radio.send(str(int(speed)))
+
+	speed *= AIR_RESISTANCE
+	sleep(20)

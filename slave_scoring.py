846105eb2235dcf53e46055d3aafa32d237e0056
mrenow <other.ezhui@gmail.com>
2017-01-10 16:29:48 +1100

    Update code for master and slave
    - Sends speed to the master
    - master receives the speed and converts it to a percentage out of 4000
    - master prints out maximum speed (for testing purposes)

--- slave_scoring.py
+++ slave_scoring.py
@@ -1,13 +1,18 @@
 from microbit import *
 import radio
-
+AIR_RESISTANCE = 0.98
 radio.on()
-
+radio.config(channel=20)
+speed = 0
 while True:
     display.clear()
-    if accelerometer.get_x() > 500:
-        print("scoring")
-        radio.send()
+    acc = accelerometer.get_x()//16
+    if acc > 31:
         display.show(Image.ARROW_N)
-    
+        speed += acc
     sleep(20)
+    radio.send(str(int(speed)))
+    speed *= AIR_RESISTANCE
+    
+    
+   
